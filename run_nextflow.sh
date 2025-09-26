5aec8cf31acc51440cbb575a438bee3db5813cb9
saundersresearch <adam.m.saunders@vanderbilt.edu>
2025-09-26 20:43:07 +0000

    Add script for running Nextflow

---
+++ run_nextflow.sh
@@ -0,0 +1,4 @@
+nextflow_dir="/fs5/p_masi/fitbir/example_datasets/example_nextflow"
+for bids_dir in $nextflow_dir/site-*; do
+    devcontainer exec --workspace-folder . nextflow run main.nf --input $bids_dir -profile docker -resume
+done
